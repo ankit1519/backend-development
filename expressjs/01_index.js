ffd33d5149865dcd1274be98db911974b09989a7
Ankit <raazankit1502@gmail.com>
2024-01-16 05:37:09 +0530

    intro to express completed

--- expressjs/01_index.js
+++ expressjs/01_index.js
@@ -1,3 +1,16 @@
 const express=require('express');
+const fs=require('fs')
 const server=express();
-server.listen(8000);
+const data=JSON.parse(fs.readFileSync('01_data.json','utf-8'))
+const product=data.products;
+server.get('/',(req,res)=>{
+    //res.send("Hello");
+    //res.send('<h1>Ankit here</h1>')
+    //res.sendFile(`C:\backend development\\nodejs\index.html`)
+    //res.json(data);
+   // res.sendStatus(404)
+   //res.status(201).send("hi")
+})
+server.listen(8000,()=>{
+    console.log("server started");
+});
